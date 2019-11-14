1cdd9f855386b76cff0518ecacf0a6b4197ea353
Celeste Morton and Liam Kenna <celeste+liam@>
2019-11-13 16:45:17 -0800

    add front end
    Co-Authored-By: Celeste Morton and Liam Kenna <celeste@>

--- src/scripts.js
+++ src/scripts.js
@@ -5,6 +5,29 @@ import './css/styles.css';
 
 $(document).ready(function(){
 
+  const boardSetup = [5,3,'','',7,'','','','',6,'','',1,9,5,'','','','',9,8,'','','','',6,'',8,'','','',6,'','','',3,4,'','',8,'',3,'','',1,7,'','','',2,'','','',6,'',6,'','','','',2,8,'','','','',4,1,9,'','',5,'','','','',8,'','',7,9];
+  console.log(boardSetup.length);
 
-  
+  const allBlanks = $(':input');
+  for (var i = 0; i < boardSetup.length; i++) {
+    if (boardSetup[i]) {
+      $(allBlanks[i]).val(boardSetup[i]);
+    }
+  }
+
+  $('button').click(function() {
+    const thisBoard = [];
+    let thisRow = [];
+    const allNumberInputs = $(':input');
+    for (var i = 0; i < allNumberInputs.length; i++) {
+      const thisNumber = parseInt($(allNumberInputs[i]).val());
+      thisRow.push(thisNumber);
+      if (thisRow.length === 9) {
+        thisBoard.push(thisRow);
+        thisRow = [];
+      }
+    }
+    console.log(thisBoard);
+
+  });
 });
